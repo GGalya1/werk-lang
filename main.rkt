a9204062c73ba93afa00ef636aee03949c27a3aa
GGalya1 <garturmail@gmail.com>
2025-09-22 23:15:20 +0200

    Update main.rkt
    1) defined first language - ArithC
    2) defined parser for ArithC
    3) defined interpreter for ArithC
    4) starts desugaring process

--- main.rkt
+++ main.rkt
@@ -2,3 +2,56 @@
 
 (display "Hello to initial commit from PLAI!")
 ; no displyln here (
+
+(define-type MisspelledAnimal
+[caml (humps : number)]
+[yacc (height : number)])
+
+(define (good? [ma : MisspelledAnimal]) : boolean
+(type-case MisspelledAnimal ma
+[caml (humps) (>= humps 2)]
+[yacc (height) (> height 2.1)]))
+
+
+; till here goes actual code, that needed
+
+; this is a core of the language. We need "desugar" to add more operations
+(define-type ArithC
+[numC (n : number)]
+[plusC (l : ArithC) (r : ArithC)]
+[multC (l : ArithC) (r : ArithC)])
+
+(define (parse [s : s-expression]) : ArithC
+(cond
+[(s-exp-number? s) (numC (s-exp->number s))]
+[(s-exp-list? s)
+(let ([sl (s-exp->list s)])
+(case (s-exp->symbol (first sl))
+[(+) (plusC (parse (second sl)) (parse (third sl)))]
+[(*) (multC (parse (second sl)) (parse (third sl)))]
+[else (error 'parse "invalid list input")]))]
+[else (error 'parse "invalid input")]))
+
+(define (interp [a : ArithC]) : number
+(type-case ArithC a
+[numC (n) n]
+[plusC (l r) (+ (interp l) (interp r))]
+[multC (l r) (* (interp l) (interp r))]))
+
+
+;; here starts desugaring
+(define-type ArithS
+[numS (n : number)]
+[plusS (l : ArithS) (r : ArithS)]
+[bminusS (l : ArithS) (r : ArithS)]
+[multS (l : ArithS) (r : ArithS)])
+
+(define (desugar [as : ArithS]) : ArithC
+(type-case ArithS as
+[numS (n) (numC n)]
+[plusS (l r) (plusC (desugar l)
+(desugar r))]
+[multS (l r) (multC (desugar l)
+(desugar r))]
+[bminusS (l r) (plusC (desugar l)
+(multC (numC -1) (desugar r)))]))
