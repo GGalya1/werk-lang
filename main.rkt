48d73b1b91837b9774184887860eba751cbc45be
GGalya1 <garturmail@gmail.com>
2025-09-25 21:58:06 +0200

    Update main.rkt
    - Removed unnecessary code
    - Added tests for addition, multiplication, and combinations thereof
    - Code is now more readable

--- main.rkt
+++ main.rkt
@@ -1,57 +1,70 @@
 #lang plai-typed
 
-(display "Hello to initial commit from PLAI!")
-; no displyln here (
-
-(define-type MisspelledAnimal
-[caml (humps : number)]
-[yacc (height : number)])
-
-(define (good? [ma : MisspelledAnimal]) : boolean
-(type-case MisspelledAnimal ma
-[caml (humps) (>= humps 2)]
-[yacc (height) (> height 2.1)]))
-
-
-; till here goes actual code, that needed
-
-; this is a core of the language. We need "desugar" to add more operations
+;; this is a core of the language. We need "desugar" to add more operations
 (define-type ArithC
-[numC (n : number)]
-[plusC (l : ArithC) (r : ArithC)]
-[multC (l : ArithC) (r : ArithC)])
+  [numC (n : number)]
+  [plusC (l : ArithC) (r : ArithC)]
+  [multC (l : ArithC) (r : ArithC)]
+)
 
 (define (parse [s : s-expression]) : ArithC
-(cond
-[(s-exp-number? s) (numC (s-exp->number s))]
-[(s-exp-list? s)
-(let ([sl (s-exp->list s)])
-(case (s-exp->symbol (first sl))
-[(+) (plusC (parse (second sl)) (parse (third sl)))]
-[(*) (multC (parse (second sl)) (parse (third sl)))]
-[else (error 'parse "invalid list input")]))]
-[else (error 'parse "invalid input")]))
+  (cond
+    [(s-exp-number? s) (numC (s-exp->number s))]
+    
+    [(s-exp-list? s)
+     (let ([sl (s-exp->list s)])
+       (case (s-exp->symbol (first sl))
+         [(+) (plusC (parse (second sl)) (parse (third sl)))]
+         [(*) (multC (parse (second sl)) (parse (third sl)))]
+         [else (error 'parse "invalid list input")]))]
+    
+    [else (error 'parse "invalid input")]
+  )
+)
 
 (define (interp [a : ArithC]) : number
-(type-case ArithC a
-[numC (n) n]
-[plusC (l r) (+ (interp l) (interp r))]
-[multC (l r) (* (interp l) (interp r))]))
+  (type-case ArithC a
+    [numC (n) n]
+    [plusC (l r) (+ (interp l) (interp r))]
+    [multC (l r) (* (interp l) (interp r))]
+  )
+)
+
 
 
 ;; here starts desugaring
 (define-type ArithS
-[numS (n : number)]
-[plusS (l : ArithS) (r : ArithS)]
-[bminusS (l : ArithS) (r : ArithS)]
-[multS (l : ArithS) (r : ArithS)])
+  [numS (n : number)]
+  [plusS (l : ArithS) (r : ArithS)]
+  [bminusS (l : ArithS) (r : ArithS)]
+  [multS (l : ArithS) (r : ArithS)]
+)
 
 (define (desugar [as : ArithS]) : ArithC
-(type-case ArithS as
-[numS (n) (numC n)]
-[plusS (l r) (plusC (desugar l)
-(desugar r))]
-[multS (l r) (multC (desugar l)
-(desugar r))]
-[bminusS (l r) (plusC (desugar l)
-(multC (numC -1) (desugar r)))]))
+  (type-case ArithS as
+    [numS (n) (numC n)]
+    [plusS (l r) (plusC (desugar l) (desugar r))]
+    [multS (l r) (multC (desugar l) (desugar r))]
+    [bminusS (l r) (plusC (desugar l) (multC (numC -1) (desugar r)))]
+  )
+)
+
+
+;;(define-type FunDefC
+;;  [fdC (name : symbol) (arg : symbol) (body : ExprC)])
+
+
+
+
+;; ===================================================
+
+;; entry point/ pipeline
+(define (run [sexp : s-expression]) : number
+  (interp (parse sexp)))
+
+;; tests
+(test (run '(+ 1 2)) 3)                           
+(test (run '(* 2 3)) 6)                          
+;(test (run '(- 5 2)) 3)                           
+;(test (run '(- (* 2 3) (+ 1 4))) 1)               
+(test (run '(+ (* 2 (+ 3 4)) (* 10 6))) 74)     
